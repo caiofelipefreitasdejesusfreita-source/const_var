9a98b611d9d44e4dd25d084c8bc2ce7a80bde1cd
caiofelipefreitasdejesusfreita-source <caiofelipefreitasdejesusfreita@gmail.com>
2026-02-20 17:11:52 -0300

    Aula var e const

---
+++ var_const.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>const_var</title>
+</head>
+<body>
+    <?php
+    /*int = numero, 
+    string = caractere, 
+    boleabo = true/ false, 
+    float = 0.0*/
+
+
+
+    $aluno = "arnoldo shuaseneguer";
+    $idade = "988 anos";
+    /*const inicia com a palavra define";
+     entre aspas dulpas
+     1º constante
+     2º valor */
+
+    define("instituicao", "centro de educaçaão tec. do amazonas");
+
+    echo "olá aluno, $aluno $idade bem vindo ao" . instituicao
+
+    
+    
+    
+    ?>
+
+</body>
+</html>
